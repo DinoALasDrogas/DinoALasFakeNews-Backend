1785c25490e1a0be4c5e86f54d19388bbc935f81
al.garcia.v <al.garcia@indexzero.io>
2021-03-06 21:42:34 -0600

    Started webhook for chatbot

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 from routes import *
 
-if __name__ == '__main__':
-    app.run(debug = True)
+# run Flask app
+if __name__ == "__main__":
+    app.run()
